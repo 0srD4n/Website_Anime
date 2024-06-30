a91085daabe3080317513a92ef96f5127d9dad90
XplDan <XplDan@proton.me>
2024-06-30 07:56:42 +0700

    update css html

---
+++ index.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("mySidebar").style = "width: 250px; border: groove;";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style = "width: 0; border = none";
+}
